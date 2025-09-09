d3271b7f4aa3d776f7ecefb4a926447a1d4f81e6
eunsikim <kimeunsik229@gmail.com>
2025-09-09 15:21:56 -0400

    in class sept 9

--- Module 2/inClass4.py
+++ Module 2/inClass4.py
@@ -8,27 +8,27 @@ def main():
 
     if user_choice != "rock" and user_choice != "paper" and user_choice != "scissors":
         print("Invalid choice")
-    # else:
-    # Random Computer choice
-    options = ['rock', 'paper', 'scissors']
-    comp_choice = random.choice(options)
-
-    print(f"Computer choice: {comp_choice}")
-
-    # Logic
-    # Win cases
-    if user_choice == "rock" and comp_choice == "scissors":
-        print("Win")
-    elif user_choice == "paper" and comp_choice == "rock":
-        print("Win")
-    elif user_choice == "scissors" and comp_choice == "paper":
-        print("Win")
-    # Draw
-    elif user_choice == comp_choice:
-        print("Draw")
-    # Lost
     else:
-        print("Lost")
+        # Random Computer choice
+        options = ['rock', 'paper', 'scissors']
+        comp_choice = random.choice(options)
+
+        print(f"Computer choice: {comp_choice}")
+
+        # Logic
+        # Win cases
+        if user_choice == "rock" and comp_choice == "scissors":
+            print("Win")
+        elif user_choice == "paper" and comp_choice == "rock":
+            print("Win")
+        elif user_choice == "scissors" and comp_choice == "paper":
+            print("Win")
+        # Draw
+        elif user_choice == comp_choice:
+            print("Draw")
+        # Lost
+        else:
+            print("Lost")
 
 if __name__ == "__main__":
     main()
