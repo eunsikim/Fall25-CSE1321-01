c0944373ac5121dda39c9eecda6b826b52f391bc
eunsikim <kimeunsik229@gmail.com>
2025-10-02 15:12:43 -0400

    in class sep 2

---
+++ Module 3/functions2.py
@@ -0,0 +1,10 @@
+def Force_Calculation():
+    force = mass * acceleration
+    return force
+
+mass = 40
+acceleration = 50
+
+print(f"Mass: {mass}")
+print(f"Acceleration: {acceleration}")
+print(f"Force: {Force_Calculation()}")
