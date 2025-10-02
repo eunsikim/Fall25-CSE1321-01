f33203c440bcb380ad807ceb58067f3d3b38779e
eunsikim <kimeunsik229@gmail.com>
2025-10-02 14:49:49 -0400

    in class sep 1

---
+++ Module 3/functions1.py
@@ -0,0 +1,31 @@
+def helloWorld():
+    print("Hello World")
+
+def addition(num1, num2):
+    op = num1 + num2
+
+    return op
+
+def printMenu():
+    print("Press 1 for Hello World")
+    print("Press 2 for Addition")
+    print("Press X to quit")
+
+    return input("> ")
+
+def main():
+    while True:
+        option = printMenu()
+
+        if option == "1":
+            helloWorld()
+        elif option == "2":
+            num1 = int(input("Enter a number: "))
+            num2 = int(input("Enter another number: "))
+
+            print(addition(num1, num2))
+        elif option == "X":
+            break
+
+if __name__ == "__main__":
+    main()
